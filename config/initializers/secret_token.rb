7d26ba920812715e085f64acef1990cc7f98bbfb
Jonathan Friedman <friedmanj98@gmail.com>
2012-05-06 19:08:39 -0500

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stalltalk::Application.config.secret_token = '1f9556050777304d1dbc082eb6a9247e91e931b5ad7fbecf5110d51b3a55374499885581c9eb71199b0d5adf686dbcddad4fd9e7434df1dd6dc0070ede0f94b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Stalltalk::Application.config.session_store :cookie_store, key: '_stalltalk_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Stalltalk::Application.config.session_store :active_record_store
