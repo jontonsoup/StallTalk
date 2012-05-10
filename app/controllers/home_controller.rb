ce4975e50883680e4c3797d17769b52f441b5037
Jonathan Friedman <friedmanj98@gmail.com>
2012-05-10 02:14:01 -0500

    Bug Fix: home redirect. Feature: View all posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
     def index
-        @poopstation_id = params[:s]
-        redirect_to :controller=>'poopstations', :action => 'show', :id => @poopstation_id
+        if is_mobile_device?
+            if not params[:s].nil?
+                @poopstation_id = params[:s]
+                redirect_to :controller=>'poopstations', :action => 'show', :id => @poopstation_id
+            end
+        end
     end
 end
