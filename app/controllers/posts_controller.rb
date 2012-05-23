d43df23671a8644ec4deb0dab2ad86fd136619bb
Jonathan Friedman <friedmanj98@gmail.com>
2012-05-22 21:30:00 -0500

    Feature: fixed default sorting of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.reverse
 
     respond_to do |format|
       format.html # index.html.erb
