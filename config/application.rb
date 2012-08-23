752aade88fa70405203ad1ec8b1d470f31a37fa9
Jonathan Friedman <friedmanj98@gmail.com>
2012-08-22 21:46:33 -0500

    Bug fix? maybe asset fic

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,7 @@ module Stalltalk
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.assets.initialize_on_precompile = false
     # Enable the asset pipeline
     config.assets.enabled = true
 
