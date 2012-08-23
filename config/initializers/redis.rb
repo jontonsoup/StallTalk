9257a0d842af604ff9e1abb2fb2de255e3b985d9
Jonathan Friedman <friedmanj98@gmail.com>
2012-08-23 17:27:59 -0500

    Bug Fix: heroku fix v2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
+ENV["REDISTOGO_URL"] ||= 'redis://redistogo:399438b16bcc825d9eb9e774a3fc4b00@char.redistogo.com:9036/'
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Resque.redis = REDIS
