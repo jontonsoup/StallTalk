35105976b20b05e04c6349445dbfbc56ad8cc0cd
Jonathan Friedman <friedmanj98@gmail.com>
2012-11-05 23:37:38 -0600

    Sign in works

--- app/models/stalltalkian.rb
+++ app/models/stalltalkian.rb
@@ -4,7 +4,7 @@ class Stalltalkian < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
